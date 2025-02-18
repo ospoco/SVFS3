bf7f5aa5554b4fdda36ad8bd1da1c5646dbf7c57
VanL <van@ospo.co>
2025-02-18 23:50:24 +0000

    Update SVFS.py

--- SVFS.py
+++ SVFS.py
@@ -18,6 +18,8 @@
 #  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 #  MA 02110-1301, USA.
 
+# Updated and ported to Python 3 by Van Lindberg, 2025
+
 import os, sys, struct, datetime, time, io, weakref
 
 
